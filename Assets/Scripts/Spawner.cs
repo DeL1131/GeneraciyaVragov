c69032e50d040dfb2ec55d9c9dbabbc62d4c1b4c
DeL1131 <artiks012@gmail.com>
2024-06-24 19:23:59 +0200

    GeneraciyaVragov

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,15 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    Enemy enemy;
     [SerializeField] private Transform[] _points;
     [SerializeField] private Enemy[] _enemys;
     [SerializeField] private float _timeSpawn;
     [SerializeField] private float _speedEnemy;
 
+    private Enemy _enemy;
     private Transform[] _targets;
 
     private void Start()
@@ -19,11 +18,11 @@ public class Spawner : MonoBehaviour
 
     private void CreateEnemy()
     {
-        enemy = _enemys[Random.Range(0, _enemys.Length)];
-        Instantiate(enemy, _points[Random.Range(0, _points.Length)].transform.position, Quaternion.identity);
+        _enemy = _enemys[Random.Range(0, _enemys.Length)];
+        Instantiate(_enemy, _points[Random.Range(0, _points.Length)].transform.position, Quaternion.identity);
         var position = new Vector3(Random.Range(-10.0f, 10.0f), 0, Random.Range(-10.0f, 10.0f));
-        enemy.GetSpeed(_speedEnemy);
-        enemy.GetTarget(position);
+        _enemy.GetSpeed(_speedEnemy);
+        _enemy.GetTarget(position);
     }
 }
     
